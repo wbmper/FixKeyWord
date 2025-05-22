51c6436675c0cbbf2936ddf7091316712962d068
wbmper <rladbwmd123@gmail.com>
2025-05-22 16:35:32 +0900

    [Refactoring] 확장성을 고려하여 알고리즘이 변경되도 가능하도록 ㅅ수정

--- mission2/fix_keyword.cpp
+++ mission2/fix_keyword.cpp
@@ -64,34 +64,43 @@ private:
 	int UZ;
 };
 
+inline int levenshtein(const std::string& a, const std::string& b) {
+	const size_t len_a = a.size();
+	const size_t len_b = b.size();
+
+	std::vector<std::vector<int>> d(len_a + 1, std::vector<int>(len_b + 1));
+
+	for (size_t i = 0; i <= len_a; ++i) d[i][0] = i;
+	for (size_t j = 0; j <= len_b; ++j) d[0][j] = j;
+
+	for (size_t i = 1; i <= len_a; ++i) {
+		for (size_t j = 1; j <= len_b; ++j) {
+			if (a[i - 1] == b[j - 1])
+				d[i][j] = d[i - 1][j - 1];
+			else
+				d[i][j] = 1 + std::min({ d[i - 1][j], d[i][j - 1], d[i - 1][j - 1] });
+		}
+	}
+	return d[len_a][len_b];
+}
+
 class SimilarityCalculator
 {
 public:
-	bool isSimilar(const std::string& keyWord1, const std::string& keyWord2)
+	bool isSimilar(const std::string& keyWord1, const std::string& keyWord2, int (*algo)(const std::string&, const std::string&))
 	{
+		if (keyWord1.empty() || keyWord2.empty()) return true;
+
+		int dist = algo(keyWord1, keyWord2);
+		int max_len = std::max(keyWord1.length(), keyWord2.length());
+		// 유사도 비율 (1.0: 완전히 같음, 0.0: 전혀 다름)
+		double similarity = 1.0 - (double)dist / max_len;
+		int score = 1 + static_cast<int>(similarity * 99);
+
+		if (score >= 80) return true;
 		return false;
 	}
 
-private:
-	int levenshtein(const std::string& a, const std::string& b) {
-		const size_t len_a = a.size();
-		const size_t len_b = b.size();
-
-		std::vector<std::vector<int>> d(len_a + 1, std::vector<int>(len_b + 1));
-
-		for (size_t i = 0; i <= len_a; ++i) d[i][0] = i;
-		for (size_t j = 0; j <= len_b; ++j) d[0][j] = j;
-
-		for (size_t i = 1; i <= len_a; ++i) {
-			for (size_t j = 1; j <= len_b; ++j) {
-				if (a[i - 1] == b[j - 1])
-					d[i][j] = d[i - 1][j - 1];
-				else
-					d[i][j] = 1 + std::min({ d[i - 1][j], d[i][j - 1], d[i - 1][j - 1] });
-			}
-		}
-		return d[len_a][len_b];
-	}
 };
 
 class InputOutputManager
@@ -188,7 +197,7 @@ class DayBestManager : public IKeywordManager
 		std::string name;
 		name.clear();
 		for (keyWordManageData& node : weekDayBest[index]) {
-			if (cal.isSimilar(node.name, keyWord)) {
+			if (cal.isSimilar(node.name, keyWord, levenshtein)) {
 				name = node.name;
 				break;
 			}
@@ -243,7 +252,7 @@ class TypeBestManager : public IKeywordManager
 		std::string name;
 		name.clear();
 		for (keyWordManageData& node : weekTypeBest[index]) {
-			if (cal.isSimilar(node.name, keyWord)) {
+			if (cal.isSimilar(node.name, keyWord, levenshtein)) {
 				name = node.name;
 				break;
 			}
