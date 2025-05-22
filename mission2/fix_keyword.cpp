1b5f0755493f7b16a211e68181564144b7bd4c35
wbmper <rladbwmd123@gmail.com>
2025-05-22 15:30:24 +0900

    [Feature] file open test 작성 및 class 뼈대 구성

--- mission2/fix_keyword.cpp
+++ mission2/fix_keyword.cpp
@@ -6,185 +6,85 @@
 #include <algorithm>
 #include "gmock/gmock.h"
 
-using namespace std;
-
-struct Node {
-	string w;
-	string wk;
+struct InputData {
+	std::string keyWord;
+	std::string weekDay;
 };
-
-struct keyWordManagData {
-	string name;
+struct keyWordManageData {
+	std::string name;
 	int point;
 
-	bool operator<(const keyWordManagData& other) const {
+	bool operator<(const keyWordManageData& other) const {
 		return point < other.point;
 	}
 };
 
-vector<keyWordManagData> weekDayBest[7]; //월 ~ 금
-vector<keyWordManagData> weekTypeBest[2]; //평일, 주말
-int UZ = 9;
-
-// 레벤슈타인 거리 계산 알고리즘 (문자열 유사도 검사)
-int levenshtein(const std::string& a, const std::string& b) {
-	const size_t len_a = a.size();
-	const size_t len_b = b.size();
-
-	std::vector<std::vector<int>> d(len_a + 1, std::vector<int>(len_b + 1));
-
-	for (size_t i = 0; i <= len_a; ++i) d[i][0] = i;
-	for (size_t j = 0; j <= len_b; ++j) d[0][j] = j;
-
-	for (size_t i = 1; i <= len_a; ++i) {
-		for (size_t j = 1; j <= len_b; ++j) {
-			if (a[i - 1] == b[j - 1])
-				d[i][j] = d[i - 1][j - 1];
-			else
-				d[i][j] = 1 + std::min({ d[i - 1][j], d[i][j - 1], d[i - 1][j - 1] });
-		}
-	}
-	return d[len_a][len_b];
-}
-
-// 점수 환산
-bool similer(const std::string& a, const std::string& b) {
-	if (a.empty() && b.empty()) return 100;
-	if (a.empty() || b.empty()) return 1;
-
-	int dist = levenshtein(a, b);
-	int max_len = std::max(a.length(), b.length());
-	// 유사도 비율 (1.0: 완전히 같음, 0.0: 전혀 다름)
-	double similarity = 1.0 - (double)dist / max_len;
-
-	int score = 1 + static_cast<int>(similarity * 99);
-
-	if (score >= 80) return true;
-	return false;
-}
-
-string input2(string w, string wk) {
-	UZ++;
-
-	int index = 0;
-	if (wk == "monday") index = 0;
-	if (wk == "tuesday") index = 1;
-	if (wk == "wednesday") index = 2;
-	if (wk == "thursday") index = 3;
-	if (wk == "friday") index = 4;
-	if (wk == "saturday") index = 5;
-	if (wk == "sunday") index = 6;
-
-	//평일 / 주말
-	int index2 = 0;
-	if (index >= 0 && index <= 4) index2 = 0;
-	else index2 = 1;
-
-	int point = UZ;
-
-	//관리 목록에 존재하는지 확인
-	//관리되는 키워드이면 점수가 증가
-
-	long long int max1 = 0;
-	long long int max2 = 0;
-
-	int flag = 0;
-	for (keyWordManagData& node : weekDayBest[index]) {
-		if (node.name == w) {
-			max1 = node.point + (node.point * 0.1);
-			node.point += (node.point * 0.1);
-			flag = 1;
-			break;
-		}
+class SimilarityCalculator
+{
+public:
+	bool isSimilar(const std::string& keyWord1, const std::string& keyWord2)
+	{
+		return false;
 	}
 
-	for (keyWordManagData& node : weekTypeBest[index2]) {
-		if (node.name == w) {
-			max2 = node.point + (node.point * 0.1);
-			node.point += (node.point * 0.1);
-			break;
-		}
-	}
+private:
+	int levenshtein(const std::string& a, const std::string& b) {
+		const size_t len_a = a.size();
+		const size_t len_b = b.size();
 
-	//재정렬 작업
-	if (UZ >= 2100000000 || max1 >= 2100000000 || max2 >= 2100000000) {
-		UZ = 9;
-		for (int i = 0; i < 5; i++) {
-			int num = 1;
-			for (keyWordManagData& node : weekDayBest[i]) {
-				node.point = num;
-				num++;
+		std::vector<std::vector<int>> d(len_a + 1, std::vector<int>(len_b + 1));
+
+		for (size_t i = 0; i <= len_a; ++i) d[i][0] = i;
+		for (size_t j = 0; j <= len_b; ++j) d[0][j] = j;
+
+		for (size_t i = 1; i <= len_a; ++i) {
+			for (size_t j = 1; j <= len_b; ++j) {
+				if (a[i - 1] == b[j - 1])
+					d[i][j] = d[i - 1][j - 1];
+				else
+					d[i][j] = 1 + std::min({ d[i - 1][j], d[i][j - 1], d[i - 1][j - 1] });
 			}
 		}
-		for (int i = 0; i < 2; i++) {
-			int num = 1;
-			for (keyWordManagData& node : weekTypeBest[i]) {
-				node.point = num;
-				num++;
-			}
+		return d[len_a][len_b];
+	}
+};
+
+class InputOutputManager
+{
+public:
+	bool openData(const std::string& fileName) {
+		std::ifstream fin{ fileName };
+
+		if (!fin.is_open()) {
+			std::cerr << "파일을 열 수 없습니다." << std::endl;
+			return false;
 		}
+
+		return true;
 	}
-
-	if (flag == 1) {
-		return w;
+	void updateInputData() {
 	}
-
-
-	//찰떡 HIT
-	for (keyWordManagData& node : weekDayBest[index]) {
-		if (similer(node.name, w)) {
-			return node.name;
-		}
+	void printOutput() {
 	}
+private:
+	std::vector<InputData> input;
+};
 
-	for (keyWordManagData& node : weekTypeBest[index]) {
-		if (similer(node.name, w)) {
-			return node.name;
-		}
-	}
+class IKeywordManager
+{
+public:
+	virtual void initializePoint() = 0;
+	virtual void registerData() = 0;
+	virtual bool getPerfectKeyword() = 0;
+	virtual bool getSimilarKeyword() = 0;
+private:
+	std::vector<keyWordManageData> data;
+};
 
-	//완벽 HIT / 찰떡 HIT 둘다 아닌경우
-	if (weekDayBest[index].size() < 10) {
-		weekDayBest[index].push_back({ w, point });
-		std::sort(weekDayBest[index].begin(), weekDayBest[index].end());
-	}
+class DayBestManager : public IKeywordManager
+{
+};
 
-	if (weekTypeBest[index].size() < 10) {
-		weekTypeBest[index].push_back({ w, point });
-		std::sort(weekTypeBest[index].begin(), weekTypeBest[index].end());
-	}
-
-	if (weekDayBest[index].size() == 10) {
-		if (weekDayBest[index].back().point < point) {
-			weekDayBest[index].pop_back();
-			weekDayBest[index].push_back({ w, point });
-			std::sort(weekDayBest[index].begin(), weekDayBest[index].end());
-		}
-	}
-
-	if (weekTypeBest[index].size() == 10) {
-		if (weekTypeBest[index].back().point < point) {
-			weekTypeBest[index].pop_back();
-			weekTypeBest[index].push_back({ w, point });
-			std::sort(weekTypeBest[index].begin(), weekTypeBest[index].end());
-		}
-	}
-
-	return w;
-}
-
-void input() {
-	ifstream fin{ "keyword_weekday_500.txt" }; //500개 데이터 입력
-	for (int i = 0; i < 500; i++) {
-		string t1, t2;
-		fin >> t1 >> t2;
-		string ret = input2(t1, t2);
-		std::cout << ret << "\n";
-	}
-}
-
-int main() {
-	input();
-	testing::InitGoogleMock();
-	return RUN_ALL_TESTS();
-}
+class TypeBestManager : public IKeywordManager
+{
+};
--- mission2/test.cpp
+++ mission2/test.cpp
@@ -1,6 +1,22 @@
 #include "gmock/gmock.h"
+#include "fix_keyword.cpp"
 
-TEST(Group, TC)
+class Fixture : public ::testing::Test
 {
-	EXPECT_EQ(1, 1);
+public:
+	std::string filename = "keyword_weekday_500.txt";
+	std::string wrongFilename = "adf.txt";
+	InputOutputManager ioManager;
+};
+
+TEST_F(Fixture, InputOutManager에서파일오픈한다)
+{
+	EXPECT_EQ(ioManager.openData(filename), true);
+	EXPECT_EQ(ioManager.openData(wrongFilename), false);
+}
+
+int main()
+{
+	testing::InitGoogleMock();
+	return RUN_ALL_TESTS();
 }
